fcd79568945704edb07cc878c605a902a367c76e
ErvinOwusu <ervin.owusu@student.region16ct.org>
2022-12-14 22:08:43 -0500

    Functioning filter to products, added new js file, edited faq page and added new styling page for products

--- javascript.js
+++ javascript.js
@@ -5,3 +5,45 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 })
+
+
+var checkMetal = document.getElementById('metal');
+var checkWood = document.getElementById('wood');
+
+var chess = document.getElementById('chess');
+var axe = document.getElementById('axe');
+var sign = document.getElementById('sign');
+
+function metal() {
+    var checkMetal = document.getElementById('metal');
+    var checkWood = document.getElementById('wood');
+
+    var chess = document.getElementById('chess');
+    var axe = document.getElementById('axe');
+    var sign = document.getElementById('sign');
+
+    if (checkMetal.checked == true) {
+
+    }
+    else {
+
+    }
+}
+
+function wood() {
+    var checkMetal = document.getElementById('metal');
+    var checkWood = document.getElementById('wood');
+
+    var chess = document.getElementById('chess');
+    var axe = document.getElementById('axe');
+    var sign = document.getElementById('sign');
+
+    if (checkWood.checked == true) {
+        chess.style.display = "none";
+        sign.style.display = "none";
+    }
+    else {
+        chess.style.display = "block";
+        sign.style.display = "block";
+    }
+}
